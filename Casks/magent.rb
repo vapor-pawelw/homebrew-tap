a9b39cd31bb4e33efc04418065ff4f50302e54dd
Paweł W <47155744+vapor-pawelw@users.noreply.github.com>
2026-03-31 22:49:36 +0200

    Add magent cask (consolidated from homebrew-magent)

---
+++ Casks/magent.rb
@@ -0,0 +1,21 @@
+cask "magent" do
+  version "1.4.0"
+  sha256 "fc3bfcbd455bf809d96b98c8c3a2c34e343e18261e24a5bf163efbecf98bf7b3"
+
+  url "https://github.com/vapor-pawelw/magent-releases/releases/download/v1.4.0/Magent.dmg"
+  name "mAgent"
+  desc "Native macOS app for managing coding agents as parallel git worktree sessions"
+  homepage "https://github.com/vapor-pawelw/magent"
+
+  app "Magent.app"
+
+  preflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{staged_path}/Magent.app"]
+  end
+
+  zap trash: [
+    "~/Library/Application Support/Magent",
+    "~/Library/Preferences/com.magent.app.plist",
+  ]
+end
