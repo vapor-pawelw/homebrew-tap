3ad6ac24d3effca9efc58fe5b4106097dbfccc37
Paweł Wojtkowiak <pawel.wojtkowiak@activerse.app>
2026-04-02 12:06:30 +0200

    Update disk-space-helper cask to 1.0.1

--- Casks/disk-space-helper.rb
+++ Casks/disk-space-helper.rb
@@ -1,6 +1,6 @@
 cask "disk-space-helper" do
-  version "1.0.0"
-  sha256 "37b1a2f62b9d45c605b610c679774ffa811dbfc1abdcdb4acd84cc6f4867e317"
+  version "1.0.1"
+  sha256 "85677cffa0759cf58199524bff83982067e41a5a71576a76386acc6ff9776f2c"
 
   url "https://github.com/vapor-pawelw/disk-space-helper/releases/download/v#{version}/DiskSpaceHelper-#{version}.dmg"
   name "Disk Space Helper"
